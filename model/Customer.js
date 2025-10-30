6a405af8a5923719f9726d3d4a512273a1ddc66d
education-bootcamp <education.seekerscloud@gmail.com>
2025-10-30 20:53:57 +0530

    init

--- model/Customer.js
+++ model/Customer.js
@@ -1,66 +1,62 @@
-const fs= require('fs').promises;
-const path = require('path');
-const dataFile = path.join(__dirname, '../customers.json');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
 
-async function readData(){
-    try{
-        const data = await fs.readFile(dataFile, 'utf-8');
-        return JSON.parse(data);
-    }catch (err){
-        return [];
-    }
-}
-async function writeData(customers){
-    await fs.writeFile(dataFile, JSON.stringify(customers,null, 2))
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 }
 
 class Customer {
-    constructor(name, address, salary) {
-        this.id = nextId++;
-        this.name = name;
-        this.address = address;
-        this.salary = salary;
-    }
 
     static async create(data) {
-        let customers = await readData();
-        const newCustomer = {
-            id: customers.length ? customers[customers.length-1].id+1:1,
-            name: data.name,
-            address: data.address,
-            salary: data.salary
-        }
-        customers.push(newCustomer);
-        await writeData(customers);
-        return newCustomer;
+        const conn = await mysql.createConnection(dbConfig);
+        const [result] = await conn.execute(
+            "INSERT INTO customer (name, address, salary) VALUES (?,?,?)",
+            [data.name, data.address, data.salary]
+        );
+
+        await conn.end();
+        return {id: result.insertId, ...data};
     }
 
     static async findAll() {
-        console.log(await readData());
-        return await readData();
+        const conn = await mysql.createConnection(dbConfig);
+        const [rows] = await conn.execute("SELECT * FROM customer");
+
+        await conn.end();
+        return rows;
     }
 
     static async findById(id) {
-        let customers = await readData();
-        return customers.find(c => c.id === id);
+        const conn = await mysql.createConnection(dbConfig);
+        const [rows] = await conn.execute("SELECT * FROM customer WHERE id=?",[id]);
+
+        await conn.end();
+        return rows[0];
     }
 
     static async update(id, data) {
-        let customers = await readData();
-        let selectedIndex = customers.findIndex(c => c.id === id);
-        if (selectedIndex=== -1) return null;
-        customers[selectedIndex]={...customers[selectedIndex], ...data};
-        await writeData(customers);
-        return customers[selectedIndex];
+        const conn = await mysql.createConnection(dbConfig);
+        const [result] = await conn.execute(
+            "UPDATE customer SET name=?, address=?, salary=? WHERE id=?",
+            [data.name, data.address, data.salary, id]
+        );
+
+        await conn.end();
+        return result.affectedRows ? {id, ...data}:null;
     }
 
     static async delete(id){
-        const customers = await readData();
-        let selectedIndex = customers.findIndex(c=>c.id===id);
-        if(selectedIndex===-1) return null;
-        const deletedData = customers.splice(selectedIndex,1)[0];
-        await writeData(customers);
-        return deletedData;
+        const conn = await mysql.createConnection(dbConfig);
+        const [result] = await conn.execute(
+            "DELETE FROM customer WHERE id=?",
+            [id]
+        );
+
+        await conn.end();
+        return result.affectedRows ? true:null;
     }
 }
 
