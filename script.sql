b02e2ff90fb7d6b1cc3257ae5bb669df1fc58c78
Kehan Hansala <165448314+kehan-hansala@users.noreply.github.com>
2025-11-02 22:53:32 +0530

    update

---
+++ script.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS mern_5;
+USE mern_5;
+CREATE TABLE IF NOT EXISTS customer(
+    id INT AUTO_INCREMENT PRIMARY KEY ,
+    name VARCHAR(100),
+    address VARCHAR(200),
+    salary DECIMAL(10,2)
+);
